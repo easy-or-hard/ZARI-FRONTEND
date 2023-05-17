5b95cf8cda279709283ae8fc89acd7a5866f56dd
이요 <krb7korea@me.com>
2023-05-17 18:33:24 +0900

    타입 오류 수정

--- src/components/zari/zari.tsx
+++ src/components/zari/zari.tsx
@@ -21,6 +21,13 @@ export default function Zari({zariId: id}: { zariId: number }) {
     switch (response.statusCode) {
         case 200: // 정상 케이스
             zari = (response as OkResponseDto<IncludeBanzzackZariDto>).data;
+            // TODO, zari 객체에 대한 접근이 초기화되기전에 접근하다는 오류가 나오지만 문제 없음. 하지만 IDE 에서 오류 표시를 안 할 수 있도록 수정하기
+            return (
+                <div>
+                    <div>자리 아이디: {zari.id}</div>
+                    {zari?.banzzacks.map(banzzacks => <Banzzack key={banzzacks.id} banzzackId={banzzacks.id}/>)}
+                </div>
+            )
             break;
         case 401: // 비인증 사용자
             redirect('/sign-in');
@@ -28,13 +35,7 @@ export default function Zari({zariId: id}: { zariId: number }) {
         case 404: // 별을 생성하지 않음, 별 생성과 동시에 자리가 생성됩니다.
             redirect('/byeol/create');
             break;
+        default:
+            return <div>Error: Unexpected status code</div>; // Default error message
     }
-
-    // TODO, zari 객체에 대한 접근이 초기화되기전에 접근하다는 오류가 나오지만 문제 없음. 하지만 IDE 에서 오류 표시를 안 할 수 있도록 수정하기
-    return (
-        <div>
-            <div>자리 아이디: {zari.id}</div>
-            {zari?.banzzacks.map(banzzacks => <Banzzack key={banzzacks.id} banzzackId={banzzacks.id}/>)}
-        </div>
-    )
 }
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,7 +1,8 @@
 export const JWT = {
-    ACCESS_TOKEN: process.env.JWT_ACCESS_TOKEN,
+    ACCESS_TOKEN: process.env.JWT_ACCESS_TOKEN as string,
 }
 
+
 export class API {
-    static BASE_URL = process.env.API_BASE_URL;
+    static BASE_URL = process.env.API_BASE_URL as string;
 }
