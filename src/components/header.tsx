53e2b150310e5b243ecc8fb027a72a90af1e07cd
이요 <krb7korea@me.com>
2023-05-22 22:55:00 +0900

    샘플 헤더 컴포넌트

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header
+            className='absolute top-0 left-0 w-full h-16 flex justify-center items-center'
+        >
+            헤더
+        </header>
+    )
+}
