fb46442c6c8bdbd2843d13271ccdde0672c53e59
이요 <krb7korea@me.com>
2023-05-22 23:06:53 +0900

    백그라운드 컬러 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,6 @@ module.exports = {
         buttonDisabled: '#464646',
         buttonDisabledTextColor: '#BDBDBD',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
