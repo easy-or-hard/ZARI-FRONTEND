0304d5ecb4107878d8d06b45d49f6fb2bc20fc94
이요 <krb7korea@me.com>
2023-05-22 23:03:46 +0900

    반짝이가 붙었을 때 이펙트 컴포넌트

---
+++ src/components/banzzack/effect-banzzack.tsx
@@ -0,0 +1,20 @@
+export default function EffectBanzzack({positionX, positionY, r}: { positionX: number, positionY: number, r: number }) {
+    return (
+        <>
+            <defs>
+                <radialGradient id="radial-gradient" gradientUnits="objectBoundingBox">
+                    <stop offset="0" stop-color="#fff"/> {/* 중앙 색상을 밝게 지정 */}
+                    <stop offset="1" stop-color="rgba(255, 255, 255, 0)"/> {/* 테두리 색상을 투명으로 지정 */}
+                </radialGradient>
+            </defs>
+            <g>
+                <ellipse cx={positionX} cy={positionY} rx={r / 3} ry={r * 3} fill="url(#radial-gradient)"/>
+                <ellipse cx={positionX} cy={positionY} rx={r * 3} ry={r / 3} fill="url(#radial-gradient)"/>
+                <g transform={`rotate(-45 ${positionX} ${positionY})`}>
+                    <ellipse cx={positionX} cy={positionY} rx={r / 3} ry={r * 10} fill="url(#radial-gradient)"/>
+                    <ellipse cx={positionX} cy={positionY} rx={r * 10} ry={r / 3} fill="url(#radial-gradient)"/>
+                </g>
+            </g>
+        </>
+    )
+}
