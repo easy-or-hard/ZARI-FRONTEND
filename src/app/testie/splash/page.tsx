772b413a9109a03258db0a01be0501bc904955c8
이요 <krb7korea@me.com>
2023-05-22 23:06:29 +0900

    테스트용 스플래시

---
+++ src/app/testie/splash/page.tsx
@@ -0,0 +1,10 @@
+import Splash from "@/components/splash";
+
+export default function Test() {
+    return (
+        <>
+            <div>Ang Ki mo Ti</div>
+            <Splash/>
+        </>
+    )
+}
