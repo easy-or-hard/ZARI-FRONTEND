69a93153e7cddb8d4728cc594ab063f38c95153b
이요 <krb7korea@me.com>
2023-05-22 22:44:12 +0900

    예외 처리 조건문 제거

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,30 +1,13 @@
-import React from "react";
-import {cookies} from "next/headers";
-import {JWT} from "@/lib/const";
-import {redirect} from "next/navigation";
-import UserService from "@/service/user/user.service";
-import {OkResponseDto} from "@/service/common/dto/ok.response.dto";
-import {UserEntity} from "@/service/user/dto/entities/user.entity";
-import SignInComponent from "@/app/auth/sign-in/sign-in";
+import SignInList from "@/components/auth/signInList";
+import {Identity} from "@/components/Identity";
 
-export default async function SignIn() {
-    const jwt = cookies().get(JWT.ACCESS_TOKEN);
-    const {key, fetcher} = UserService.findMe(jwt);
-    const response = await fetcher(key);
-
-    switch (response.statusCode) {
-        case 200: // 정상 응답
-            if ((response as OkResponseDto<UserEntity>).data.byeolId) {
-                redirect('/byeol/me');
-            }
-            break;
-        case 401: // 비인증 사용자, 비인증 사용자는 이 화면을 봐야하기 때문에 아무것도 안 합니다.
-            break;
-    }
-
+export default function SignInComponent() {
     return (
-        <>
-            <SignInComponent/>
-        </>
+        <div className='h-full flex flex-col justify-center'>
+            <div className='h-1/2 flex flex-col justify-center'>
+                <Identity/>
+            </div>
+            <div className='h-1/2'><SignInList/></div>
+        </div>
     )
 }
