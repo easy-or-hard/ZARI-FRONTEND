f06ae328c7894dfc67eb1be5b856d84101f6a37b
이요 <krb7korea@me.com>
2023-05-25 06:30:51 +0900

    랠러티브 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ children,
         <html lang="kr">
         <body className={'bg-gray-950'}>
         <main
-            className="min-h-[480px] max-h-[926px] w-full min-w-[270px] max-w-[428px] mx-auto"
+            className="relative min-h-[480px] max-h-[926px] w-full min-w-[270px] max-w-[428px] mx-auto"
             style={{
                 backgroundImage: `linear-gradient(to bottom, rgba(19, 16, 29, 0.7), rgba(63, 44, 83, 0.2)), url(/BG_Star.png)`,
                 backgroundPosition: 'center, top left', // 각각의 위치 설정
