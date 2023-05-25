edcbba2bb25eadc4ee71890b838a889af14dda05
이요 <krb7korea@me.com>
2023-05-25 22:46:46 +0900

    애니메이션 추가 애니메이션 종료시 dom이 사라지도록 수정

--- src/components/ui/popup/modal-popup.tsx
+++ src/components/ui/popup/modal-popup.tsx
@@ -1,31 +1,46 @@
-import CloseIcon from "@/components/ui/icon/size24/close";
-import {ReactNode} from "react";
+'use client'
+
+import {ReactNode, useCallback, useEffect, useMemo, useState} from "react";
 import Dim from "@/components/ui/popup/dim";
+import CloseIcon from "@/components/ui/icon/size24/close";
 
 type Props = {
     children: ReactNode;
-    title: string;
-    setToggle: (toggle: boolean) => void;
+    setVisible: (toggle: boolean) => void;
 }
-export default function ModalPopup({children, title, setToggle}: Props) {
+
+export default function ModalPopup({children, setVisible} : Props) {
+    const [visibleAnimation, setVisibleAnimation] = useState(true);
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+
+        // 컴포넌트가 unmount 될 때 원래 스크롤 상태로 되돌리기
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, []);
+
+    const animationClass = useMemo(() => (
+        visibleAnimation
+            ? 'animate-jump-in animate-duration-300 animate-ease-in-out'
+            : 'animate-jump-out animate-duration-300 animate-ease-in-out'
+    ), [visibleAnimation]);
+
+    const close = useCallback(() => {
+        if (!visibleAnimation) {
+            setVisible(false);
+        }
+        // setVisibleToggle 은 의존성에 주입할 필요가 없습니다.
+    }, [visibleAnimation]);
+
     return (
         <>
-            <div className={'absolute z-20 top-0 left-0 p-10 w-full h-full flex flex-col justify-center items-center'}>
-                <div
-                    className={'p-4 gap-2 rounded-2xl w-full bg-zari_default_black flex flex-col justify-center items-center'}>
-                    <div className={'w-full flex justify-between'}>
-                        {title}
-                        <button onClick={() =>setToggle(false)}>
-                            <CloseIcon/>
-                        </button>
-                    </div>
-                    <div className={'p-4 rounded-2xl w-full bg-zari_btn_disabled'}>
-                        {children}
-                    </div>
-                    <button
-                        className="py-3 w-full bg-zari_primary rounded-2xl">
-                        버튼
-                    </button>
+            <div className={'absolute z-20 top-0 p-10 w-full h-full flex flex-col justify-center items-center'}>
+                <div className={`${animationClass} border-[1px] border-white border-opacity-20 relative w-full bg-zari_default_black bg-opacity-60 backdrop-blur-lg rounded-2xl p-3`}
+                     onAnimationEnd={close}
+                >
+                    <button className={'absolute m-3 top-0 right-0'} onClick={() => setVisibleAnimation(false)}><CloseIcon/></button>
+                    {children}
                 </div>
             </div>
             <Dim/>
