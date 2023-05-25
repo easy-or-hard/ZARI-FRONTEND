2c341aae0279a66cc28f0e387913a60c0cce89b6
이요 <krb7korea@me.com>
2023-05-25 14:54:14 +0900

    부모딤을 이쪽으로 옮기고, 토글 함수 프롭으로 전달받게 수정

--- src/components/ui/popup/modal-popup.tsx
+++ src/components/ui/popup/modal-popup.tsx
@@ -1,27 +1,34 @@
 import CloseIcon from "@/components/ui/icon/size24/close";
 import {ReactNode} from "react";
+import Dim from "@/components/ui/popup/dim";
 
 type Props = {
     children: ReactNode;
     title: string;
+    setToggle: (toggle: boolean) => void;
 }
-export default function ModalPopup({children, title}: Props) {
+export default function ModalPopup({children, title, setToggle}: Props) {
     return (
-        <div
-            className={'p-4 gap-2 rounded-2xl w-full bg-zari_default_black opacity-80 flex flex-col justify-center items-center'}>
-            <div className={'w-full flex justify-between'}>
-                {title}
-                <button>
-                    <CloseIcon/>
-                </button>
+        <>
+            <div className={'absolute z-20 top-0 left-0 p-10 w-full h-full flex flex-col justify-center items-center'}>
+                <div
+                    className={'p-4 gap-2 rounded-2xl w-full bg-zari_default_black flex flex-col justify-center items-center'}>
+                    <div className={'w-full flex justify-between'}>
+                        {title}
+                        <button onClick={() =>setToggle(false)}>
+                            <CloseIcon/>
+                        </button>
+                    </div>
+                    <div className={'p-4 rounded-2xl w-full bg-zari_btn_disabled'}>
+                        {children}
+                    </div>
+                    <button
+                        className="py-3 w-full bg-zari_primary rounded-2xl">
+                        버튼
+                    </button>
+                </div>
             </div>
-            <div className={'p-4 rounded-2xl w-full bg-zari_btn_disabled'}>
-                {children}
-            </div>
-            <button
-                className="py-3 w-full bg-zari_primary rounded-2xl">
-                버튼
-            </button>
-        </div>
+            <Dim/>
+        </>
     )
 }
