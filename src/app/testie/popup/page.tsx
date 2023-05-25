1238e55b082815322355b3d992b4de9d42d6feee
이요 <krb7korea@me.com>
2023-05-25 14:54:41 +0900

    토글함수 전달

--- src/app/testie/popup/page.tsx
+++ src/app/testie/popup/page.tsx
@@ -1,20 +1,15 @@
 'use client'
 
-import Dim from "@/components/ui/popup/dim";
 import ModalPopup from "@/components/ui/popup/modal-popup";
 import {useState} from "react";
 import {EnableButton} from "@/components/button/enableButton";
 
 export default function PopUpPage() {
-    const [show, setShow] = useState(false);
+    const [toggle, setToggle] = useState(false);
     return (
         <>
-            <EnableButton onClick={() => setShow(true)}>팝업</EnableButton>
-            {show && <div
-                className={'relative z-20 p-10 w-full h-full flex flex-col justify-center items-center'}
-            ><ModalPopup title={'앙키모'}>다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파.</ModalPopup></div>
-            }
-            {show && <Dim/>}
+            <EnableButton onClick={() => setToggle(true)}>팝업</EnableButton>
+            {toggle &&<ModalPopup title={'앙키모'} setToggle={setToggle}>다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파. 다람쥐 헌 쳇바귀 돌고파.</ModalPopup>}
         </>
     )
 }
