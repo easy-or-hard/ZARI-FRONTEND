b67e2d87a4dbf7da4d5ddbe52976e974d5e9049c
이요 <krb7korea@me.com>
2023-05-26 20:20:01 +0900

    flex를 grid로 변경

--- src/components/ui/menu/sub-menu.tsx
+++ src/components/ui/menu/sub-menu.tsx
@@ -4,7 +4,7 @@ import SignOutIcon from "@/components/ui/icon/size24/sign-out";
 export default function SubMenu() {
     return (
         <span
-            className={'bg-zari_default_white rounded-full inline-flex flex-col items-center justify-center'}
+            className={`px-3 py-4 bg-zari_default_white rounded-full inline-grid gap-4`}
         >
             <button
                 className={'bg-zari_default_white w-11 h-11 rounded-full flex items-center justify-center'}
