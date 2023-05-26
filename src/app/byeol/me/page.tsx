aad03a27863ce11236b2c0d20651fdf8f50bcae3
이요 <krb7korea@me.com>
2023-05-27 00:22:25 +0900

    데이터 가져와서 에러 처리하는 로직 수정

--- src/app/byeol/me/page.tsx
+++ src/app/byeol/me/page.tsx
@@ -3,21 +3,37 @@
 import ByeolService from "@/services/byeol/byeol.service";
 import {useRouter} from "next/navigation";
 import useSWR from "swr";
-import ZariPreview from "@/components/zari/zariPreview";
+import {useEffect} from "react";
+import {ZariError} from "@/services/common/fetcher";
 
-const {key, fetcher} = ByeolService.getMeFetcher();
-
+const {key, fetcher} = ByeolService.findMeFetcher();
 export default function MePage() {
     const router = useRouter();
-    const {data: response, isLoading, error} = useSWR(key, fetcher);
+    const {error, data, isLoading} = useSWR(key, fetcher);
 
-    if (error) {
-        console.error('error', error);
-        router.replace('/auth/sign-in');
-    } else if (isLoading) {
-        console.info('isLoading', isLoading);
-        return (<>is Loading</>);
-    }
+    useEffect(() => {
+        if (error) {
+            if (error instanceof ZariError) {
+                switch (error.statusCode) {
+                    case 401:
+                        router.replace('/auth/sign-in');
+                        break;
+                    case 404:
+                        router.replace('/byeol/create');
+                }
+            } else {
+                // TODO, 예외 에러에 대해서 어떻게 처리할까?
+                console.error(error);
+                throw error;
+            }
+        } else if (data) {
+            console.log(data.data.zaris)
+            if (data.data.zaris.length === 1) {
+                const responseData = data.data.zaris[0];
+                router.replace(`/byeol/${responseData.byeolId}`);
+            }
+        }
+    }, [data, error])
 
     if (isLoading) {
         return (
