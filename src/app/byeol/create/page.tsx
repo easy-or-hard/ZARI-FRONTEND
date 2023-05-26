ae52762d211139ff47059fd5428af64e95bd36db
이요 <krb7korea@me.com>
2023-05-27 01:09:51 +0900

    별 가입 api 호출 로직 변경

--- src/app/byeol/create/page.tsx
+++ src/app/byeol/create/page.tsx
@@ -6,6 +6,7 @@ import ByeolNameInput from "@/components/byeol/byeolNameInput";
 import BirthdaySelect from "@/components/byeol/birthday.select";
 import React, {useCallback, useState} from "react";
 import {EnableButton} from "@/components/button/enableButton";
+import {ZariError} from "@/services/common/fetcher";
 
 export default function CreatePage() {
     const router = useRouter();
@@ -13,17 +14,30 @@ export default function CreatePage() {
     const [isSelectedBirthday, setIsSelectedBirthday] = useState(false);
 
     const join = useCallback(async (form: FormData) => {
-        const {response, responseJson} = await ByeolService.createFetcher(form);
-
-        if (!response.ok) {
-            console.info('statusCode: ', responseJson.statusCode)
-            console.info('message: ', responseJson.message)
-
-            router.replace('/byeol/me');
-            return;
+        const {key, fetcher} = ByeolService.createFetcher(form);
+        try {
+            // 가입 성공시에는 반환 데이터가 없기 때문에 메소드 실행만 합니다.
+            await fetcher(key);
+            router.replace(`/byeol/me`);
+        } catch (error) {
+            // 가입 실패
+            if (error instanceof ZariError) {
+                switch (error.statusCode) {
+                    case 400: // 이미 별이 있는 유저
+                        router.replace('/byeol/me');
+                        break;
+                    case 401: // 인증되지 않은 유저
+                        router.replace('/auth/sign-in');
+                        break;
+                    case 409: // 중복된 이름
+                        // TODO, 이름이 중복되었을 때 어떻게 처리할까? 이름이 중복 되었습니다. 토스트를 띄울까?
+                        break;
+                    default:
+                        console.error(error);
+                        throw error;
+                }
+            }
         }
-
-        router.replace('/byeol/me');
     }, [router]);
 
     return (
