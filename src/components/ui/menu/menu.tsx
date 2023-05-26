0b624d4b1781429eafc0139d97e1f01e18ece1f9
이요 <krb7korea@me.com>
2023-05-26 19:53:04 +0900

    외부 모듈을 사용한 애니메이션 제거, 트랜지션으로 교체

--- src/components/ui/menu/menu.tsx
+++ src/components/ui/menu/menu.tsx
@@ -1,51 +1,46 @@
 'use client'
 
-import {useSpring, animated} from '@react-spring/web';
+import {useCallback, useEffect, useState} from "react";
 import PlusIcon from "@/components/ui/icon/size24/plus";
-import {useEffect, useState} from "react";
 import SubMenu from "@/components/ui/menu/sub-menu";
 
 export default function Menu() {
-    const [toggle, setToggle] = useState(false);
-    const [subMenuToggle, setSubMenuToggle] = useState(false);
-    const [timer, setTimer] = useState(null);
-    const {transform: menuTransform} = useSpring({
-        transform: toggle ? 'rotate(45deg)' : 'rotate(0deg)',
-    });
+    const [menuButtonRotate, setMenuButtonRotate] = useState(false);
+    const [subMenuVisible, setSubMenuVisible] = useState(false);
 
-    const {transform: subMenuTransform, opacity: subMenuOpacity} = useSpring({
-        transform: toggle ? 'translateY(58px)' : 'translateY(0px)',
-        opacity: toggle ? 1 : 0,
-    });
+    const menuAnimation = `transition-transform ${menuButtonRotate ? 'rotate-45' : ''}`;
+    const subMenuAnimation = `transition ${menuButtonRotate ? '' : '-translate-y-1 opacity-0'}`;
 
     useEffect(() => {
-        // setTimer 대신 이벤트 기반 콜백으로 처리하고 싶은데,
-        // 애니메이션에 콜백을 넣는 방법을 몰라서, 타이머로 할 수 밖에 없는듯...
-        if (toggle) {
-            setSubMenuToggle(true);
-            // @ts-ignore, 타입을 못 찾겠습니다...
-            clearTimeout(timer);
-        } else {
-            // @ts-ignore, 타입을 못 찾겠습니다...
-            setTimer(setTimeout(() => setSubMenuToggle(false), 500));
+        if (menuButtonRotate) {
+            setSubMenuVisible(true);
         }
-        // 디펜던시에 timer 를 넣으면 무한 반복합니다. 넣지마세요.
-    }, [toggle])
+    }, [menuButtonRotate]);
+
+    const subMenuTransitionEnd = useCallback(() => {
+        if (!menuButtonRotate) {
+            setSubMenuVisible(false);
+        }
+    }, [menuButtonRotate]);
 
     return (
-        <div className={'absolute top-0'}>
+        <span>
             <button
-                onClick={() => setToggle(!toggle)}
-                className={'absolute z-10 bg-zari_primary w-11 h-11 rounded-full flex items-center justify-center'}
+                onClick={() => setMenuButtonRotate(!menuButtonRotate)}
+                className={`${menuAnimation} z-10 bg-zari_primary w-11 h-11 rounded-full flex items-center justify-center`}
             >
-                <animated.div style={{transform: menuTransform}}>
-                    <PlusIcon/>
-                </animated.div>
+                <PlusIcon/>
             </button>
-            {subMenuToggle &&
-                <animated.div style={{transform: subMenuTransform, opacity: subMenuOpacity, position: 'absolute'}}>
+            {/* TODO, 나타날때도 트랜지션을 걸고싶은데 잘 안되네용. ㅎ*/}
+            {
+                subMenuVisible &&
+                <span
+                    className={`mt-3 inline-block translate ${subMenuAnimation}`}
+                    onTransitionEnd={subMenuTransitionEnd}
+                >
                     <SubMenu/>
-                </animated.div>}
-        </div>
+                </span>
+            }
+        </span>
     );
 }
