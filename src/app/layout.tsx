57e383fd5957f014ca6d51857aa6ba288c8af932
이요 <krb7korea@me.com>
2023-05-26 19:52:10 +0900

    배경 미지 중심점 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ children,
             className="relative min-h-[480px] max-h-[926px] w-full min-w-[270px] max-w-[428px] mx-auto"
             style={{
                 backgroundImage: `linear-gradient(to bottom, rgba(19, 16, 29, 0.7), rgba(63, 44, 83, 0.2)), url(/BG_Star.png)`,
-                backgroundPosition: 'center, top left', // 각각의 위치 설정
+                backgroundPosition: 'center, top center', // 각각의 위치 설정
                 backgroundRepeat: 'no-repeat, no-repeat', // 각각의 반복 설정
             }}
         >
