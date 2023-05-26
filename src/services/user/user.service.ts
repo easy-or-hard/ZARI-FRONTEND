db907aa472c5d2e778abed646814860dc58944f9
이요 <krb7korea@me.com>
2023-05-27 00:22:37 +0900

    주석 수정

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -5,7 +5,7 @@ import Fetcher from "@/services/common/fetcher";
 
 export default class UserService {
     /**
-     * 나의 유저 정보를 호출 합니다.
+     * 나의 유저 정보를 가져옵니다.
      * @param accessToken
      * @throws {ZariError} response.ok 가 false 일 경우 에러를 던집니다.
      * @returns {Object} key, fetcher 가 담긴 객체를 반환합니다.
