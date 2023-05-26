d71bbb63ae303b74deaa83344f49a86eea4900b7
이요 <krb7korea@me.com>
2023-05-27 00:23:20 +0900

    내 별 정보를 가져오는 페처 메소드 추가

--- src/services/byeol/byeol.service.ts
+++ src/services/byeol/byeol.service.ts
@@ -76,4 +76,26 @@ export default class ByeolService {
 
         return {response, responseJson}
     }
+
+    /**
+     * 나의 별 정보를 가져옵니다.
+     * @param accessToken
+     * @throws {ZariError} response.ok 가 false 일 경우 에러를 던집니다.
+     * @returns {Object} key, fetcher 가 담긴 객체를 반환합니다.
+     */
+    static findMeFetcher(accessToken?: RequestCookie | undefined) {
+        let init: RequestInit = {
+            method: 'GET',
+            credentials: 'include', // 이 옵션이 크로스 브라우징에도 쿠키를 전송.
+        }
+
+        accessToken?.value &&
+        Object.assign(init, {
+            headers: {
+                Cookie: `access_token=${accessToken.value};`, // front 에서 bff 를 호출할 경우 브라우저가 준 쿠키 설정
+            },
+        });
+
+        return Fetcher.FetcherFactory<IncludeZariByeolDto>({key: `${API.BASE_URL}/byeol/me`, init});
+    }
 }
