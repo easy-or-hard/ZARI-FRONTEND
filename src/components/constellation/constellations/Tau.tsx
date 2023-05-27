c4e500aa55c49437a9a5cd73222f94b916d3aba4
이요 <krb7korea@me.com>
2023-05-27 20:38:07 +0900

    신규 별자리 추가하면서 로직 순서 변경

--- src/components/constellation/constellations/Tau.tsx
+++ src/components/constellation/constellations/Tau.tsx
@@ -11,94 +11,84 @@ export default function Tau({setClickedStarNumber, setBanzzackVisible, banzzacks
     };
 
     return (
-        <>
-            <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 236.98 439.1">
-                <g id="line">
-                    <line x1="155.56" y1="24.17" x2="133.85" y2="60.19" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="128.97" y1="68.27" x2="104.98" y2="110.5" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="98.48" y1="117.5" x2="46.06" y2="171.6" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="40.13" y1="178.46" x2="37.98" y2="224.84" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="35.94" y1="233.17" x2="20" y2="266.1" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="20.83" y1="275.54" x2="45.9" y2="377.43" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="53.15" y1="379.15" x2="77.82" y2="351.46" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="86.13" y1="347.31" x2="137.52" y2="353.65" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="145.49" y1="357.94" x2="211.98" y2="414.1" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="46.5" y1="177.9" x2="96.82" y2="230.91" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                    <line x1="105.61" y1="234.07" x2="164.27" y2="227.26" fill="none" opacity=".6" stroke="#fff"
-                          strokeWidth=".5"/>
-                </g>
-                <g
-                    id="_1" data-name="1">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 1) && <EffectBanzzack cx={157.98} cy={20}/>}
-                    <circle cx="157.98" cy="20" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="157.98" cy="20" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_2" data-name="2">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 2) && <EffectBanzzack cx="130.98" cy="64.1"/>}
-                    <circle cx="130.98" cy="64.1" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="130.98" cy="64.1" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_3" data-name="3">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 3) && <EffectBanzzack cx="101.98" cy="114"/>}
-                    <circle cx="101.98" cy="114" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="101.98" cy="114" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_4" data-name="4">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 4) && <EffectBanzzack cx="42.98" cy="175"/>}
-                    <circle cx="42.98" cy="175" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="42.98" cy="175" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_5" data-name="5">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 5) && <EffectBanzzack cx="37.98" cy="229"/>}
-                    <circle cx="37.98" cy="229" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="37.98" cy="229" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_6" data-name="6">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 6) && <EffectBanzzack cx="20" cy="271.1"/>}
-                    <circle cx="20" cy="271.1" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="20" cy="271.1" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_7" data-name="7">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 7) && <EffectBanzzack cx="48.98" cy="381"/>}
-                    <circle cx="48.98" cy="381" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="48.98" cy="381" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_8" data-name="8">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 8) && <EffectBanzzack cx="81.98" cy="349"/>}
-                    <circle cx="81.98" cy="349" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="81.98" cy="349" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_9" data-name="9">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 9) && <EffectBanzzack cx="141.98" cy="355"/>}
-                    <circle cx="141.98" cy="355" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="141.98" cy="355" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_10" data-name="10">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 10) && <EffectBanzzack cx="216.98" cy="419.1"/>}
-                    <circle cx="216.98" cy="419.1" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="216.98" cy="419.1" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_11" data-name="11">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 11) && <EffectBanzzack cx="100.98" cy="233" />}
-                    <circle cx="100.98" cy="233" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="100.98" cy="233" r="20" fill="#231f20" opacity="0"/>
-                </g>
-                <g id="_12" data-name="12">
-                    {banzzacks.find(banzzack => banzzack.starNumber === 12) && <EffectBanzzack cx="168.98" cy="227"/>}
-                    <circle cx="168.98" cy="227" r="5" fill="none" opacity=".6" stroke="#fff" strokeMiterlimit="10"/>
-                    <circle cx="168.98" cy="227" r="20" fill="#231f20" opacity="0"/>
-                </g>
-            </svg>
-        </>
+        <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 360 640">
+            <g id="_11" data-name="11">
+                {banzzacks.find(banzzack => banzzack.starNumber === 11) && <EffectBanzzack cx="339.99" cy="479.13"/>}
+                <circle cx="339.99" cy="479.13" r="20" opacity="0"/>
+                <circle cx="339.99" cy="479.13" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_10" data-name="10">
+                {banzzacks.find(banzzack => banzzack.starNumber === 10) && <EffectBanzzack cx="339.99" cy="429.91"/>}
+                <circle cx="339.99" cy="429.91" r="20" opacity="0"/>
+                <circle cx="339.99" cy="429.91" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_9" data-name="9">
+                {banzzacks.find(banzzack => banzzack.starNumber === 9) && <EffectBanzzack cx="254.41" cy="412.57"/>}
+                <circle cx="254.41" cy="412.57" r="20" opacity="0"/>
+                <circle cx="254.41" cy="412.57" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_8" data-name="8">
+                {banzzacks.find(banzzack => banzzack.starNumber === 8) && <EffectBanzzack cx="210.78" cy="361.11"/>}
+                <circle cx="210.78" cy="361.11" r="20" opacity="0"/>
+                <circle cx="210.78" cy="361.11" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_7" data-name="7">
+                {banzzacks.find(banzzack => banzzack.starNumber === 7) && <EffectBanzzack cx="147.01" cy="334.26"/>}
+                <circle cx="147.01" cy="334.26" r="20" opacity="0"/>
+                <circle cx="147.01" cy="334.26" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_6" data-name="6">
+                {banzzacks.find(banzzack => banzzack.starNumber === 6) && <EffectBanzzack cx="78.77" cy="304.06"/>}
+                <circle cx="78.77" cy="304.06" r="20" opacity="0"/>
+                <circle cx="78.77" cy="304.06" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_5" data-name="5">
+                {banzzacks.find(banzzack => banzzack.starNumber === 5) && <EffectBanzzack cx="20.01" cy="221.28"/>}
+                <circle cx="20.01" cy="221.28" r="20" opacity="0"/>
+                <circle cx="20.01" cy="221.28" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_4" data-name="4">
+                {banzzacks.find(banzzack => banzzack.starNumber === 4) && <EffectBanzzack cx="260" cy="307.42"/>}
+                <circle cx="260" cy="307.42" r="20" opacity="0"/>
+                <circle cx="260" cy="307.42" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_3" data-name="3">
+                {banzzacks.find(banzzack => banzzack.starNumber === 3) && <EffectBanzzack cx="195.12" cy="290.64"/>}
+                <circle cx="195.12" cy="290.64" r="20" opacity="0"/>
+                <circle cx="195.12" cy="290.64" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_2" data-name="2">
+                {banzzacks.find(banzzack => banzzack.starNumber === 2) && <EffectBanzzack cx="183.93" cy="235.82"/>}
+                <circle cx="183.93" cy="235.82" r="20" opacity="0"/>
+                <circle cx="183.93" cy="235.82" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="_1" data-name="1">
+                {banzzacks.find(banzzack => banzzack.starNumber === 1) && <EffectBanzzack cx="115.69" cy="160.87"/>}
+                <circle cx="115.69" cy="160.87" r="20" opacity="0"/>
+                <circle cx="115.69" cy="160.87" r="5" fill="none" stroke="#fff" stroke-miterlimit="10"/>
+            </g>
+            <g id="line" opacity=".6">
+                <line x1="75.85" y1="299.89" x2="22.94" y2="225.36" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="142.38" y1="332.21" x2="83.39" y2="306.13" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="206.16" y1="359.17" x2="151.46" y2="336.16" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="251.25" y1="408.85" x2="213.95" y2="364.85" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="334.99" y1="428.85" x2="259.26" y2="413.59" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="339.99" y1="473.99" x2="339.99" y2="434.89" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="200.12" y1="291.93" x2="254.75" y2="306.06" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="184.95" y1="240.82" x2="194.11" y2="285.69" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="119.06" y1="164.57" x2="180.52" y2="232.07" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+                <line x1="256.56" y1="311.18" x2="214.25" y2="357.36" fill="none" stroke="#fff"
+                      stroke-miterlimit="10"/>
+            </g>
+        </svg>
     )
 }
