7acf45590931b76d79b05da43fa8d589a8eea6d9
이요 <krb7korea@me.com>
2023-05-27 22:27:55 +0900

    스피드 다이얼 컨텐츠 버튼에 기능 추가

--- src/components/ui/menu/sub-menu.tsx
+++ src/components/ui/menu/sub-menu.tsx
@@ -1,19 +1,27 @@
+'use client'
+
 import HomeIcon from "@/components/ui/icon/size24/home";
 import SignOutIcon from "@/components/ui/icon/size24/sign-out";
 import SettingIcon from "@/components/ui/icon/size24/setting";
+import {useRouter} from "next/navigation";
+import {API} from "@/lib/const";
 
 export default function SubMenu() {
+    const router = useRouter();
     return (
         <span
             className={`px-3 py-4 bg-zari_default_white rounded-full inline-grid gap-4`}
         >
-            <button>
+            <button onClick={() => router.push('/byeol/me')}>
                 <HomeIcon color={'#161616'}/>
             </button>
-            <button>
+            <button >
                 <SettingIcon color={'#161616'}/>
             </button>
-            <button>
+            <button onClick={() => fetch(`${API.BASE_URL}/auth/sign-out`, {
+                method: 'GET',
+                credentials: 'include'
+            })}>
                 <SignOutIcon color={'#161616'}/>
             </button>
         </span>
