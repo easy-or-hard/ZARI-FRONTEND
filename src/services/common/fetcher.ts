b286ce61f3c01645ade2fa49ccfb19dd08d03c8b
이요 <krb7korea@me.com>
2023-05-28 21:44:32 +0900

    유틸리티 메소드 추가 makeInit

--- src/services/common/fetcher.ts
+++ src/services/common/fetcher.ts
@@ -1,5 +1,6 @@
 import {NotOkResponseDto} from "@/services/common/dto/not-ok.response.dto";
 import {OkResponseDto} from "@/services/common/dto/ok.response.dto";
+import {RequestCookie} from "next/dist/compiled/@edge-runtime/cookies";
 
 /**
  * ZARI API 호출시 나오는 200, 201 이외의 에러를 처리하는 에러 클래스 입니다.
@@ -15,7 +16,25 @@ export class ZariError extends Error {
     }
 }
 
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
 export default class Fetcher {
+    static makeInit(method: Method, accessToken?: RequestCookie | undefined) {
+        const init: RequestInit = {
+            method: method,
+            credentials: 'include', // 이 옵션이 크로스 브라우징에도 쿠키를 전송.
+        }
+
+        accessToken?.value &&
+        Object.assign(init, {
+            headers: {
+                Cookie: `access_token=${accessToken.value};`, // front 에서 bff 를 호출할 경우 브라우저가 준 쿠키 설정
+            }
+        });
+
+        return init;
+    }
+
     /**
      * 페처를 만드는 팩토리 메소드 입니다.
      * @param key http url 을 입력하세요.
