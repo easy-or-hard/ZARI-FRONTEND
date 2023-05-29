529787186deb6e48b3db7d98ce3d28f13f036cdb
이요 <krb7korea@me.com>
2023-05-29 19:16:04 +0900

    미들웨어로 처리하는 로직 제거

--- src/app/byeol/me/page.tsx
+++ src/app/byeol/me/page.tsx
@@ -1,33 +1,13 @@
 'use client'
 
-import {useRouter} from "next/navigation";
-import {useEffect} from "react";
-import {ZariError} from "@/services/common/fetcher";
 import {useByeolFindMe} from "@/services/byeol/byeol.use";
-import {useIsByeol} from "@/services/auth/auth.use";
 
 export default function MePage() {
-    const router = useRouter();
-    const {data: okResponseIsByeol, isLoading: isLoadingIsByeol, error: errorIsByeol} = useIsByeol();
     const {
         data: okResponseIncludeZariByeolDto,
         isLoading: isLoadingIncludeZariByeol,
         error: errorIncludeZariByeol
     } = useByeolFindMe();
-
-    useEffect(() => {
-        if (errorIsByeol) {
-            if (errorIsByeol instanceof ZariError) {
-                if (errorIsByeol.statusCode === 401) {
-                    router.push('/auth/sign-in');
-                }
-            } else {
-                // TODO, 예외 에러에 대해서 어떻게 처리할까?
-                console.error(errorIsByeol);
-                throw errorIsByeol;
-            }
-        }
-    }, [errorIsByeol]);
 
     return (
         <>
