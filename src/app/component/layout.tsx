021767e693e9c44e711f78bc4ca4e125944eaae5
이요 <krb7korea@me.com>
2023-05-30 18:40:35 +0900

    라인 정리

--- src/app/component/layout.tsx
+++ src/app/component/layout.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ToastProvider from "@/app/component/ui/toast-message/toast-provider";
 
-export default function ComponentLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+export default function ComponentLayout({children}: { children: React.ReactNode }) {
     return (
         <ToastProvider>
             {children}
