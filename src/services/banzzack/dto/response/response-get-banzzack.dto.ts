67c6ee3d82234638157160679c7d975fc414e291
이요 <krb7korea@me.com>
2023-05-30 21:39:05 +0900

    참조하는 곳이 없어서 삭제

--- src/services/banzzack/dto/response/response-get-banzzack.dto.ts
+++
@@ -1,6 +0,0 @@
-import {NotOkResponseDto} from "@/services/common/dto/not-ok.response.dto";
-import {BanzzackEntity} from "@/services/banzzack/entities/banzzack.entity";
-
-export type ResponseGetBanzzackDto = NotOkResponseDto & {
-    data: BanzzackEntity;
-}
