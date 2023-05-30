1bed5e76d601ce5f94c3f9dc0550bce17a114161
이요 <krb7korea@me.com>
2023-05-31 04:05:53 +0900

    🚀 feat(modal): 여러 팝업을 위한 배열 관리 구현
    - 팝업이 여러개 나올 수 있도록 modalStack 배열 추가
    - pushModal 과 popModal 함수 구현하여 배열 관리
    - allCloseModal 함수 구현하여 모달 팝업을 한번에 닫기

--- src/app/component/ui/popup/modal/base.modal.tsx
+++ src/app/component/ui/popup/modal/base.modal.tsx
@@ -1,62 +1,56 @@
 'use client'
 
-import {ReactNode, useContext, useEffect, useMemo, useState} from "react";
+import {useContext, useEffect, useMemo} from "react";
 import {BaseModalContext} from "@/app/component/ui/popup/modal.provider";
-import {CSSTransition} from 'react-transition-group';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import Dim from "@/app/component/ui/popup/modal/dim";
 
 /**
  * 모달의 기본 레이아웃
  * TODO, CSSTransition 이 findDOMNode 를 사용하고 있는데, 이는 deprecated 되었다. 추후에 수정 필요.
  * TODO, 매우긴 텍스트가 공백없이 들어가면 레이아웃이 길어지며 화면밖을 벗어남
+ * TODO, 모달이 여러개 뜨면 뒤에 있는 모달을 연하게 바꿔야함
  * @constructor
  */
 export default function BaseModal() {
-    // BaseModalContext 임포트
     const modalContext = useContext(BaseModalContext);
     if (!modalContext) {
         throw new Error('ModalContext is null');
     }
-    const {modalContent} = useMemo(() => modalContext, [modalContext]);
-
-    const [prevModalContent, setPrevModalContent] = useState<ReactNode | null>(null);
+    const {modalStack} = useMemo(() => modalContext, [modalContext]);
 
     useEffect(() => {
-        modalContent && setPrevModalContent(modalContent);
-    }, [modalContent]);
-
-    useEffect(() => {
-        if (modalContent) {
+        if (modalStack.length > 0) {
             document.body.style.overflow = 'hidden';
         } else {
             document.body.style.overflow = 'unset';
         }
-    }, [modalContent]);
+    }, [modalStack]);
 
     return (
-        <>
-            <CSSTransition
-                in={!!modalContent}
-                timeout={300}
-                classNames="modal"
-                unmountOnExit
-            >
-                <div className={'z-20 absolute top-0 p-10 w-full h-full grid place-items-center'}>
-                    <div
-                        className={`relative modal border-[1px] border-white border-opacity-20 w-full bg-zari_default_black bg-opacity-60 backdrop-blur-lg rounded-2xl p-3`}
-                    >
-                        {prevModalContent}
+        <TransitionGroup>
+            {modalStack.length > 0 && (
+                <CSSTransition
+                    timeout={300}
+                    classNames="dim"
+                >
+                    <Dim/>
+                </CSSTransition>
+            )}
+            {modalStack.map((modalContent, index) => (
+                <CSSTransition
+                    key={index}
+                    timeout={300}
+                    classNames="modal"
+                >
+                    <div className={`absolute top-0 px-10 w-full h-full grid place-items-center`}>
+                        <div
+                            className={`relative modal border-[1px] border-white border-opacity-20 w-full bg-zari_default_black backdrop-blur-lg rounded-2xl p-3`}>
+                            {modalContent}
+                        </div>
                     </div>
-                </div>
-            </CSSTransition>
-            <CSSTransition
-                in={!!modalContent}
-                timeout={300}
-                classNames="dim"
-                unmountOnExit
-            >
-                <Dim/>
-            </CSSTransition>
-        </>
+                </CSSTransition>
+            ))}
+        </TransitionGroup>
     )
 }
