9f3e44e5743c12febdfa387f93f631165a57764d
이요 <krb7korea@me.com>
2023-06-01 04:38:33 +0900

    ✨ feat(auth): 토근 주석 추가

--- src/services/auth/auth.fetcher.ts
+++ src/services/auth/auth.fetcher.ts
@@ -5,6 +5,7 @@ import { OkResponseDto } from "@/services/common/dto/ok.response.dto";
 
 /**
  * @description OAuth 인증한 상태인지 확인하기 위한 메소드 입니다.
+ * @param {RequestCookie} accessToken
  * @throws {ZariError} response.ok 가 false 일 경우 에러를 던집니다.
  * @return {Object} key, fetcher 가 담긴 객체를 반환합니다.
  */
@@ -17,7 +18,7 @@ const isUser = (accessToken?: RequestCookie) => {
 
 /**
  * ZARI 서비스를 사용할 수 있는 상태(별) 인지 확인합니다
- * @param {RequestCookie } accessToken
+ * @param {RequestCookie} accessToken
  * @return {Object} key, fetcher 가 담긴 객체를 반환합니다.
  */
 const isByeol = (accessToken?: RequestCookie) => {
