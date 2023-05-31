b6e2af563e1e166c3a3d740d4e13a82ef386b9b4
이요 <krb7korea@me.com>
2023-06-01 00:48:15 +0900

    🚀 feat: 변수을 데이터 dto 와 같도록 변경

--- src/app/byeol/me/page.tsx
+++ src/app/byeol/me/page.tsx
@@ -4,19 +4,23 @@ import { JWT } from "@/const";
 import ZariCarousel from "@/app/component/byeol/zari.carousel";
 import AgrOriginPath from "@/app/component/constellation/constellations/agr-origin.path";
 
+/**
+ * @description 별자리를 볼 수 있는 페이지
+ * @constructor
+ */
 export default async function MePage() {
   const jwt = cookies().get(JWT.ACCESS_TOKEN);
   const { key, fetcher } = byeolFetcher.findMe(jwt);
-  const { data } = await fetcher(key);
+  const { data: includeZariByeol } = await fetcher(key);
 
   return (
     <>
-      <div>{data.name} 가 소유중인 별자리</div>
+      <div>{includeZariByeol.name} 가 소유중인 별자리</div>
       <ZariCarousel>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 360 640">
           <AgrOriginPath />
         </svg>
-        {data.zaris.map((zari) => (
+        {includeZariByeol.zaris.map((zari) => (
           <div className={"w-full"} key={zari.id}>
             {zari.constellationIAU}
           </div>
