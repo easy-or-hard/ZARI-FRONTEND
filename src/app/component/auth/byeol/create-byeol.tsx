00244fe37a79eb2d2c228686eff7c2c014e5a316
이요 <krb7korea@me.com>
2023-06-08 17:24:36 +0900

    가입 화면 진입 로직 변경

--- src/app/component/auth/byeol/create-byeol.tsx
+++ src/app/component/auth/byeol/create-byeol.tsx
@@ -12,7 +12,7 @@ import React, {
 import ConfirmButton from "@/app/component/ui/button/confirm/confirm.button";
 import useByeol from "@/services/byeol/use.byeol";
 import { PostByeolDto } from "@/services/byeol/dto/reuqest/post-byeol.dto";
-import { useIsByeol, useIsUser } from "@/services/auth/use.auth";
+import { useIsByeol } from "@/services/auth/use.auth";
 import { JoinStepControlContext } from "@/app/component/auth/join-step-control.provider";
 
 /**
@@ -31,8 +31,7 @@ export default function CreateByeol() {
   const [isNameAvailable, setIsNameAvailable] = useState(false);
   const [isSelectedBirthday, setIsSelectedBirthday] = useState(false);
   const { data, post } = useByeol(name ? { name } : undefined);
-  const { data: isUser } = useIsUser();
-  const { data: isByeol, mutate } = useIsByeol();
+  const { data: isByeol } = useIsByeol();
 
   const join = useCallback(
     async (event: FormEvent<HTMLFormElement>) => {
@@ -45,16 +44,15 @@ export default function CreateByeol() {
       ) as unknown as PostByeolDto;
 
       await post(postByeolDto);
-      await mutate();
     },
     [post]
   );
 
   useEffect(() => {
-    if (isUser && isByeol) {
+    if (isByeol) {
       setStep(-1);
     }
-  }, [isByeol, isUser, setStep]);
+  }, [isByeol, setStep]);
 
   return (
     <form
