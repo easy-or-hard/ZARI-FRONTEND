77f2749299382d41c1e36d2e7e9ddf6f9662078d
이요 <krb7korea@me.com>
2023-06-17 00:01:36 +0900

    컬러 코드 변경

--- src/component/root/team-introduce.tsx
+++ src/component/root/team-introduce.tsx
@@ -5,25 +5,25 @@ export default function TeamIntroduce() {
   return (
     <div className={styles.wrapTeam}>
       <div className={styles.wrapTitle}>
-        <div className={styles.subTitle}>team</div>
-        <div className={styles.title}>Easy or Hard</div>
+        <div>team</div>
+        <div>Easy or Hard</div>
       </div>
       <ul className={styles.wrapMembers}>
-        <li className={styles.warpMember}>
+        <li className={styles.wrapMember}>
           <div className={styles.wrapMemberInfo}>
             <div className={styles.teamName}>XIYO</div>
             <div className={styles.teamPosition}>developer</div>
           </div>
           <ChatIcon />
         </li>
-        <li className={styles.warpMember}>
+        <li className={styles.wrapMember}>
           <div className={styles.wrapMemberInfo}>
             <div className={styles.teamName}>BUTTER ROLL</div>
             <div className={styles.teamPosition}>designer</div>
           </div>
           <ChatIcon />
         </li>
-        <li className={styles.warpMember}>
+        <li className={styles.wrapMember}>
           <div className={styles.wrapMemberInfo}>
             <div className={styles.teamName}>CHOGANGYEOL</div>
             <div className={styles.teamPosition}>developer</div>
