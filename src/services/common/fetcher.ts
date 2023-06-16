9e82853074fac40c5bbb460dd87791901df45c70
이요 <krb7korea@me.com>
2023-06-17 04:28:13 +0900

    반환 타입이 무조건 존재하도록 백엔드 변경

--- src/services/common/fetcher.ts
+++ src/services/common/fetcher.ts
@@ -32,15 +32,11 @@ export const baseFetcher = async <T>(
     throw error;
   }
 
-  try {
-    const responseJson = await response.json();
-    if (!response.ok) {
-      throw new ZariError(responseJson);
-    }
-    return responseJson as T;
-  } catch (error) {
-    // TODO 바디가 없을 경우에 대해서는 어떻게 처리할지 고민하기
+  const responseJson = await response.json();
+  if (!response.ok) {
+    throw new ZariError(responseJson);
   }
+  return responseJson as T;
 };
 
 export const baseFetcherOptions = (
