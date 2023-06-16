ffee05a4ff1f4a99992d6cea365d1c7545b83cf3
이요 <krb7korea@me.com>
2023-06-17 05:00:24 +0900

    타입으로 변경

--- src/services/zari/use.zari.ts
+++ src/services/zari/use.zari.ts
@@ -1,10 +1,10 @@
-import { getZariUrl } from "@/services/byeol/api.byeol";
+import { getZariUrl, ZariUniqueKey } from "@/services/byeol/api.byeol";
 import useSWR from "swr";
 import { baseFetcher, ZariError } from "@/services/common/fetcher";
 import useSWRMutation from "swr/mutation";
 import { ZariEntity } from "@/services/zari/entities/zari.entity";
 
-export function useZari(key: [name: string, constellationIAU: string]) {
+export function useZari(key: ZariUniqueKey) {
   return useSWR(key, (key) => {
     const url = getZariUrl(key);
     return baseFetcher<ZariEntity>(url);
