7a66a083a95ad01261ce12dc74abdbf2b47f444e
이요 <krb7korea@me.com>
2023-06-19 03:39:29 +0900

    별 이펙트 변경

--- src/component/banzzack/effect-banzzack.tsx
+++ src/component/banzzack/effect-banzzack.tsx
@@ -1,57 +1,87 @@
-import styles from "./effect-banzzack.module.css";
-
-// TODO, 현재 반짝이를 읽음 플래그가 없어서 isRead 를 옵셔널로 했습니다. 반드시 BFF 플래그를 추가하고 수정하세요.
 type Props = {
   cx: number | string;
   cy: number | string;
-  isRead?: boolean;
+  r?: number | string;
 };
 
-export default function EffectBanzzack({ cx, cy, isRead = false }: Props) {
-  const r = isRead ? 1.5 : 2;
-
+export default function EffectBanzzack({ cx, cy, r = 10 }: Props) {
   // svg 의 값이 string 이므로 number 로 변환
   cx = +cx;
   cy = +cy;
+  r = +r;
+
+  const largeRadius = r + 15;
+  const mediumRadius = r + 10;
+  const smallRadius = r + 5;
+  const fixedRadius = 1.5;
 
   return (
     <>
       <defs>
-        <radialGradient id="gradient" gradientUnits="objectBoundingBox">
-          <stop offset="0" stopColor="#fff" /> {/* 중앙 색상을 밝게 지정 */}
-          <stop offset="5" stopColor="rgba(255, 255, 255, 0)" />
-          {/* 테두리 색상을 투명으로 지정 */}
+        <radialGradient id="radial" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+          <stop offset="0%" stopColor="white" stopOpacity={1} />
+          <stop offset="100%" stopColor="white" stopOpacity={0} />
         </radialGradient>
-        <filter id="star-blur">
-          <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+        <filter id="myGradient" x="-20" y="-20" width="140" height="140">
+          <feGaussianBlur
+            in="SourceGraphic"
+            stdDeviation="5"
+            result="blurred1"
+          />
+          <feGaussianBlur
+            in="SourceGraphic"
+            stdDeviation=".5"
+            result="blurred2"
+          />
+          <feBlend in="blurred1" in2="blurred2" mode="normal" />
+        </filter>
+        <filter id="circleBlur" x="-20" y="-20" width="140" height="140">
+          <feGaussianBlur
+            in="SourceGraphic"
+            stdDeviation="2"
+            result="blurred1"
+          />
         </filter>
       </defs>
-      <g filter={"url(#star-blur)"} className={styles.pulse}>
-        <g>
-          <ellipse cx={cx} cy={cy} rx={r * 3} ry={r * 5} />
-          <ellipse cx={cx} cy={cy} rx={r * 5} ry={r * 3} />
+      <circle
+        cx={cx}
+        cy={cy}
+        r={smallRadius}
+        fill={"url(#radial)"}
+        filter={"url(#circleBlur)"}
+      />
+      <g id="star_L">
+        <g fill="url(#radial)" filter="url(#myGradient)">
+          <ellipse cx={cx} cy={cy} rx={fixedRadius} ry={largeRadius} />
+          <ellipse cx={cx} cy={cy} rx={largeRadius} ry={fixedRadius} />
         </g>
-        <g transform={`rotate(-45 ${cx} ${cy})`}>
-          <ellipse cx={cx} cy={cy} rx={r * 3} ry={r * 8} />
-          <ellipse cx={cx} cy={cy} rx={r * 8} ry={r * 3} />
+        <g
+          fill="url(#radial)"
+          transform={`rotate(45, ${cx}, ${cy})`}
+          filter="url(#myGradient)"
+        >
+          <ellipse cx={cx} cy={cy} rx={fixedRadius} ry="25" />
+          <ellipse cx={cx} cy={cy} rx="25" ry={fixedRadius} />
         </g>
-      </g>
-      <g fill="url(#gradient)">
-        <g transform={`rotate(67.5 ${cx} ${cy})`}>
-          <ellipse cx={cx} cy={cy} rx={r / 3} ry={r * 7} />
-          <ellipse cx={cx} cy={cy} rx={r * 7} ry={r / 3} />
+        <g
+          fill="url(#radial)"
+          transform={`rotate(22.5, ${cx}, ${cy})`}
+          filter="url(#myGradient)"
+        >
+          <g>
+            <ellipse cx={cx} cy={cy} rx={fixedRadius} ry={mediumRadius} />
+            <ellipse cx={cx} cy={cy} rx={mediumRadius} ry={fixedRadius} />
+          </g>
         </g>
-        <g transform={`rotate(22.5 ${cx} ${cy})`}>
-          <ellipse cx={cx} cy={cy} rx={r / 3} ry={r * 5} />
-          <ellipse cx={cx} cy={cy} rx={r * 5} ry={r / 3} />
-        </g>
-        <g transform={`rotate(45 ${cx} ${cy})`}>
-          <ellipse cx={cx} cy={cy} rx={r / 3} ry={r * 10} />
-          <ellipse cx={cx} cy={cy} rx={r * 10} ry={r / 3} />
-        </g>
-        <g transform={`rotate(90 ${cx} ${cy})`}>
-          <ellipse cx={cx} cy={cy} rx={r / 3} ry={r * 10} />
-          <ellipse cx={cx} cy={cy} rx={r * 10} ry={r / 3} />
+        <g
+          fill="url(#radial)"
+          transform={`rotate(67.5, ${cx}, ${cy})`}
+          filter="url(#myGradient)"
+        >
+          <g>
+            <ellipse cx={cx} cy={cy} rx={fixedRadius} ry={smallRadius} />
+            <ellipse cx={cx} cy={cy} rx={smallRadius} ry={fixedRadius} />
+          </g>
         </g>
       </g>
     </>
