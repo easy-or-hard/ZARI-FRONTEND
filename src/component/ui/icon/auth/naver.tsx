a01ef9eae47ba6c555b5d3fd425d673a775f8071
이요 <krb7korea@me.com>
2023-06-19 03:36:57 +0900

    아이콘 추가

---
+++ src/component/ui/icon/auth/naver.tsx
@@ -0,0 +1,16 @@
+export default function NaverIcon() {
+  return (
+    <svg
+      width="15"
+      height="17"
+      viewBox="0 0 15 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 16.5V0.5H4.64067L10.2064 8.92857V0.5H15V16.5H10.2829L4.95031 8.5V16.5H0Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
