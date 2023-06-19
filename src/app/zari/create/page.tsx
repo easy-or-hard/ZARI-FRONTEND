5b2b8496c13d91ffcd5554ef870d44525a3b47dd
이요 <krb7korea@me.com>
2023-06-20 02:34:59 +0900

    라우팅 옵션 추가

--- src/app/zari/create/page.tsx
+++ src/app/zari/create/page.tsx
@@ -2,32 +2,43 @@
 
 import constellationMap from "@/component/constellation/map";
 import Carousel from "@/component/carousel/carousel";
-import { createElement, useCallback, useRef, useState } from "react";
+import { createElement, useCallback, useEffect, useRef, useState } from "react";
 import styles from "./zari-create.module.css";
 import ConfirmButton from "@/component/ui/button/confirm/confirm.button";
 import { useRouter } from "next/navigation";
 import { usePostZari } from "@/services/zari/use.zari";
 import { useMyByeol } from "@/services/byeol/use.byeol";
+import { useIsByeol } from "@/services/auth/use.auth";
 
 export default function ZariCreatePage() {
   const router = useRouter();
   const [constellationIAU, setConstellationIAU] = useState<string>("");
   const [isClicked, setIsClicked] = useState<boolean>(false);
-  const { data: byeol } = useMyByeol();
+  const { data: isByeol } = useIsByeol();
+  const { data: myByeol } = useMyByeol(isByeol);
   // TODO, useSWR 은 key 가 없을 경우 자동으로 처리해주는데, useSWRMutation 은 그런기능이 없음....
   // 그래서 "" 를 넣어줌 업데이트 내역 반드시 확인하고 안전하게 데이터 처리할것
-  const { trigger } = usePostZari([byeol?.name ?? "", constellationIAU]);
-
+  console.log("??myByeol", myByeol);
+  const { trigger } = usePostZari([myByeol?.name ?? "", constellationIAU]);
   const sliderRef = useRef<HTMLDivElement>(null);
-
   const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isByeol || !myByeol) {
+      return;
+    }
+
+    if (myByeol.zaris.length > 0) {
+      router.replace("/byeol");
+    }
+  }, [isByeol, myByeol, router]);
 
   const handleOnClick = useCallback(() => {
     setIsClicked(true);
     trigger().then(() => {
-      router.push("/byeol");
+      router.replace("/byeol");
     });
-  }, [constellationIAU, router, trigger]);
+  }, [router, trigger]);
 
   return (
     <div className={styles.container}>
