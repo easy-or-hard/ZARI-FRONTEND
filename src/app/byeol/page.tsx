2b963cfd1199a1d21881141d582a8f0ca159f5c4
이요 <krb7korea@me.com>
2023-06-20 23:14:20 +0900

    반짝이가 없는 사용자에 대해서 가이드 팝업 추가

--- src/app/byeol/page.tsx
+++ src/app/byeol/page.tsx
@@ -2,15 +2,22 @@
 
 import Zari from "@/component/zari/zari";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { useMyByeol } from "@/services/byeol/use.byeol";
 import { useIsByeol } from "@/services/auth/use.auth";
+import { ModalContext } from "@/component/ui/popup/modal/modal.provider";
 
 /**
  * @description 별자리를 볼 수 있는 페이지
  * @constructor
  */
 export default function MyPage() {
+  const modalContext = useContext(ModalContext);
+  if (!modalContext) {
+    throw new Error("ModalContext is null");
+  }
+  const { showReadBanzzackModal } = useMemo(() => modalContext, [modalContext]);
+
   const router = useRouter();
   const { data: isByeol } = useIsByeol();
   const { data: myByeol } = useMyByeol(isByeol);
@@ -22,8 +29,12 @@ export default function MyPage() {
 
     if (myByeol.zaris.length === 0) {
       router.push("/zari/create");
+    } else {
+      showReadBanzzackModal({
+        banzzackTreeUniqueKey: ["0", "0", 0],
+      });
     }
-  }, [isByeol, myByeol, router]);
+  }, [isByeol, myByeol, router, showReadBanzzackModal]);
 
   if (!myByeol) return;
   else if (myByeol.zaris.length === 0) return;
