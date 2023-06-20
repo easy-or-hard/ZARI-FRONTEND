7c743898fb303e1b1a0980a33e0c89c16674f0ce
이요 <krb7korea@me.com>
2023-06-21 02:23:36 +0900

    SWR 키 변경

--- src/services/auth/api.auth.ts
+++ src/services/auth/api.auth.ts
@@ -1,13 +1,9 @@
 import { baseFetcher, baseFetcherOptions } from "@/services/common/fetcher";
 import { mutate } from "swr";
-import {
-  getMyByeolUrl,
-  IS_BYEOL_URL,
-  SIGN_OUT_URL,
-} from "@/services/byeol/api.byeol";
+import { IS_BYEOL_URL, SIGN_OUT_URL } from "@/services/byeol/api.byeol";
 
 export const signOut = async () => {
-  baseFetcher(SIGN_OUT_URL, baseFetcherOptions("GET"));
-  mutate(IS_BYEOL_URL, undefined, false);
-  mutate(getMyByeolUrl(), undefined, false);
+  await baseFetcher(SIGN_OUT_URL, baseFetcherOptions("GET"));
+  mutate(IS_BYEOL_URL, null, false);
+  mutate(["me"], null, false);
 };
